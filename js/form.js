655bb78bc327e394c024e650ed082c9c1fd52337
nik1224 <0lezgin5@gmail.com>
2022-11-08 17:05:42 +0500

    добавляет код

---
+++ js/form.js
@@ -0,0 +1,3 @@
+import './thumbs.js';
+
+
--- js/thumbs.js
+++ js/thumbs.js
@@ -8,7 +8,7 @@ const picturesUsersTemplate = document.querySelector('#picture')
 
 const similarPhotos = createObjects();
 
-const similarPhotosAbove = function() {
+const similarPhotosAbove = () => {
   similarPhotos.forEach((user) => {
     const pictureElement = picturesUsersTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = user.url;
@@ -18,5 +18,28 @@ const similarPhotosAbove = function() {
     usersPhotos.appendChild(pictureElement);
   });
 };
+
+const uploadFiles = document.getElementById('upload-file');
+const formEditImg = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const uploadFilesClose = document.getElementById('upload-cancel');
+
+uploadFiles.addEventListener('change', () => {
+  formEditImg.classList.remove('hidden');
+  body.classList.add('modal-open');
+});
+
+uploadFilesClose.addEventListener('click', () => {
+  formEditImg.classList.add('hidden');
+  body.classList.remove('modal-open');
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    formEditImg.classList.add('hidden');
+    body.classList.remove('modal-open');
+  }
+});
 
 export {similarPhotosAbove};
