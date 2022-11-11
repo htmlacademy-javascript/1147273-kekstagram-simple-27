cda62c9bcd0b7bbe38e867e023fb606cf6c9dbde
Keks <keks@htmlacademy.ru>
2022-11-11 19:00:07 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/form.js
@@ -0,0 +1,35 @@
+import {resetScale} from './scale.js';
+import {resetEffects} from './effect.js';
+
+const uploadFile = document.getElementById('upload-file');
+const formEditImg = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
+const formUpload = document.querySelector('.img-upload__form');
+const closeOpenModal = document.getElementById('upload-cancel');
+
+const onPopupEscKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    formEditImg.classList.add('hidden');
+    body.classList.remove('modal-open');
+    document.removeEventListener('keydown', onPopupEscKeydown);
+  }
+};
+
+const setFormHandlers = () => {
+  uploadFile.addEventListener('change', () => {
+    formEditImg.classList.remove('hidden');
+    body.classList.add('modal-open');
+    document.addEventListener('keydown', onPopupEscKeydown);
+  });
+  closeOpenModal.addEventListener('click', () => {
+    formEditImg.classList.add('hidden');
+    body.classList.remove('modal-open');
+    formUpload.reset();
+    resetScale();
+    resetEffects();
+    document.removeEventListener('keydown', onPopupEscKeydown);
+  });
+};
+
+export {setFormHandlers};
---
+++ 7/js/main.js
@@ -0,0 +1,7 @@
+import {createObjects} from './create-object.js';
+import {createSimilarPhotosAbove} from './thumbs.js';
+import {setFormHandlers} from './form.js';
+
+createObjects();
+createSimilarPhotosAbove();
+setFormHandlers();
